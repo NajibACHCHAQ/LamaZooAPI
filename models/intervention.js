e9778e7040cc9b422c46df4acd5a9bebe2e61cd7
NajibACHCHAQ <najib.achchaq@gmail.com>
2023-11-13 21:18:14 +0100

    ajout du mapping bdd

--- models/intervention.js
+++ models/intervention.js
@@ -1,6 +1,8 @@
-// models/Intervention.js
 const Sequelize = require('sequelize');
 const sequelize = require('../config/sequelize');
+const Animal = require('./animal');
+const Veterinaire = require('./veterinaire');
+const Soigneur = require('./soigneur');
 
 const Intervention = sequelize.define('intervention', {
   id: {
@@ -8,12 +10,19 @@ const Intervention = sequelize.define('intervention', {
     primaryKey: true,
     autoIncrement: true,
   },
-  date: Sequelize.DATE, // Assurez-vous que le type de données correspond à la date dans votre base de données
-  animalId: Sequelize.INTEGER, // Remplacez par le type de données correct, tel que 'Sequelize.INTEGER'
-  veterinaireId: Sequelize.INTEGER, // Même chose ici
-  soigneurId: Sequelize.INTEGER, // Même chose ici
+  date: Sequelize.DATE,
   description: Sequelize.STRING,
   designation: Sequelize.STRING,
 });
+
+// Associations
+Intervention.belongsTo(Animal, { foreignKey: 'animalId' });
+Animal.hasMany(Intervention, { foreignKey: 'animalId' });
+
+Intervention.belongsTo(Veterinaire, { foreignKey: 'veterinaireId' });
+Veterinaire.hasMany(Intervention, { foreignKey: 'veterinaireId' });
+
+Intervention.belongsTo(Soigneur, { foreignKey: 'soigneurId' });
+Soigneur.hasMany(Intervention, { foreignKey: 'soigneurId' });
 
 module.exports = Intervention;
--- models/veterinaire.js
+++ models/veterinaire.js
@@ -1,6 +1,6 @@
-// models/Soigneur.js
 const Sequelize = require('sequelize');
 const sequelize = require('../config/sequelize');
+const Intervention = require('./intervention');
 
 const Veterinaire = sequelize.define('veterinaire', {
   id: {
@@ -13,7 +13,11 @@ const Veterinaire = sequelize.define('veterinaire', {
   specialty: Sequelize.STRING,
   username: Sequelize.STRING,
   password: Sequelize.STRING,
-  mail:Sequelize.STRING,
+  mail: Sequelize.STRING,
 });
+
+// Associations
+Veterinaire.hasMany(Intervention, { foreignKey: 'veterinaireId' });
+Intervention.belongsTo(Veterinaire, { foreignKey: 'veterinaireId' });
 
 module.exports = Veterinaire;
